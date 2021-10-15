d7ce0ff1fc9ad32b2c2c8152bbefe4ef859d2a64
Alexandra <bguevaraale@gmail.com>
2021-10-15 18:02:54 -0300

    Mudanças na galeria

---
+++ js/botaoAvanca.js
@@ -0,0 +1,13 @@
+const galleryRow= document.querySelector(".gallery-row");
+const galleryRow2= document.querySelector(".gallery-row2");
+const btnPrev= document.querySelector(".btn-prev");
+const btnNext= document.querySelector(".btn-next");
+
+
+
+btnNext.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("cliquei o botão")
+
+})
+
